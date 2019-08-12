3498851a85e6bf87fd72e4f26e546c73a6a0737b
Jeremy Klein <jklein@smcpartners.com>
2019-08-11 21:08:41 -0400

    additional changes for backend db to postgres

--- webapp/app/__init__.py
+++ webapp/app/__init__.py
@@ -1,9 +1,24 @@
 # __init__.py
-import logging
+import logging, os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_oidc import OpenIDConnect
 
 logging.basicConfig(level=logging.DEBUG)
+
+def get_env_variable(name):
+    try:
+        return os.environ[name]
+    except KeyError:
+        message = "Expected environment variable '{}' not set.".format(name)
+        raise Exception(message)
+
+POSTGRES_URL = get_env_variable("POSTGRES_URL")
+POSTGRES_USER = get_env_variable("POSTGRES_USER")
+POSTGRES_PW = get_env_variable("POSTGRES_PW")
+POSTGRES_DB = get_env_variable("POSTGRES_DB")
+
+DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
 
 app = Flask(__name__)
 app.config.update({
@@ -16,9 +31,11 @@ app.config.update({
     'OIDC_USER_INFO_ENABLED': True,
     'OIDC_OPENID_REALM': 'flask-demo',
     'OIDC_SCOPES': ['openid', 'email', 'profile'],
-    'OIDC_INTROSPECTION_AUTH_METHOD': 'client_secret_post'
+    'OIDC_INTROSPECTION_AUTH_METHOD': 'client_secret_post',
+    'SQLALCHEMY_DATABASE_URI': DB_URL,
+    'SQLALCHEMY_TRACK_MODIFICATIONS': False
 })
 
-
+db = SQLAlchemy(app)
 oidc = OpenIDConnect(app)
 from . import routes
